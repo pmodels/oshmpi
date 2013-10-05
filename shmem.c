77f1ff44503aa2827336e42c616d967133482b25
Jeff Hammond <jeff.science@gmail.com>
2013-10-05 14:20:24 -0500

    restore likely macros

--- shmem.c
+++ shmem.c
@@ -19,7 +19,7 @@
 /* configuration settings */
 #define USE_ORDERED_RMA
 
-#if 0 //( defined(__GNUC__) && (__GNUC__ >= 3) ) || defined(__IBMC__) || defined(__INTEL_COMPILER) || defined(__clang__)
+#if ( defined(__GNUC__) && (__GNUC__ >= 3) ) || defined(__IBMC__) || defined(__INTEL_COMPILER) || defined(__clang__)
 #  define unlikely(x_) __builtin_expect(!!(x_),0)
 #  define likely(x_)   __builtin_expect(!!(x_),1)
 #else
