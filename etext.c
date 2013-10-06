cb02f2a80ca5ef9c9d3512857caa2b75dc6bb8c8
Jeff Hammond <jeff.science@gmail.com>
2013-10-05 21:16:05 -0500

    etext more printing since gdb and valgrind are failing me

--- etext.c
+++ etext.c
@@ -18,12 +18,15 @@ int main(void)
     shmem_barrier_all();
 
     int them = (mype+1)%npes;
+    printf("before shmem_int_put \n");
     shmem_int_put(target, source, (size_t)n, them);
+    printf("before shmem_barrier_all \n");
     shmem_barrier_all();
     them = (mype>0 ? mype-1 : npes-1);
     for (int i=0; i<n; i++)
         if (target[i] != them)
             printf("PE %d, element %d: correct = %d, got %d \n", mype, i, them, target[i]);
 
+    printf("%d: test finished \n", mype);
     return 0;
 }
