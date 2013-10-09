4229434e2df1b10167c51b6040a0b2a2501860ff
Jeff Hammond <jeff.science@gmail.com>
2013-10-09 06:03:42 -0500

    add a comment about OpenSHMEM compat

--- shmem.h
+++ shmem.h
@@ -36,6 +36,7 @@
 typedef MPI_Aint shmem_offset_t;
 /* -- end changes -- */
 
+/* SHMEM_CMP_## are not defined in OpenSHMEM */
 #define SHMEM_CMP_EQ 1
 #define SHMEM_CMP_NE 2
 #define SHMEM_CMP_GT 3
