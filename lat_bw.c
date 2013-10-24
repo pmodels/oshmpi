9eafb5d947679c7bcb9148bba9fefa29851daa1f
sg0 <sayandeep52@gmail.com>
2013-10-24 02:18:40 -0500

    added random ring test, just runs thats about it

--- lat_bw.c
+++ lat_bw.c
@@ -106,7 +106,7 @@ void ping_pong_lbw(int lo_rank,
 }
 
 /* long* msg_size bytes is PUT from left rank to right rank
- * in the fashion of a natural ring
+ * in the fashion of a natural ring (ordered according to PE ranks)
  */
 void natural_ring_lbw (unsigned int msg_size /* actual msg size is 2^msg_size */)
 {
@@ -188,6 +188,123 @@ void natural_ring_lbw (unsigned int msg_size /* actual msg size is 2^msg_size */
 
 }
 
+/* Taken from: http://stackoverflow.com/questions/6127503/shuffle-array-in-c
+ */
+/* Arrange the N elements of ARRAY in random order.
+   Only effective if N is much smaller than RAND_MAX;
+   if this may not be the case, use a better random
+   number generator. */
+void shuffle(int *array, size_t n)
+{
+	if (n > 1) 
+	{
+		size_t i;
+		for (i = 0; i < n - 1; i++) 
+		{
+			size_t j = i + rand() / (RAND_MAX / (n - i) + 1);
+			array[i] ^= array[j]; 
+			array[j] ^= array [i]; 
+			array [i] ^= array [j];
+		}
+	}
+	return;
+}
+/* long* msg_size bytes is PUT from left rank to right rank
+ * in the fashion of a ring (ranks ordered in a random fashion)
+ */
+void random_ring_lbw (unsigned int msg_size /* actual msg size is 2^msg_size */)
+{
+	unsigned int nelems = (msg_size);
+	unsigned int left_rank, right_rank;
+	double time_end, time_start;
+	if (_world_rank == 0)
+		printf("Message size: %lu\n", nelems*sizeof(long));
+
+	for (int i = 0; i < _SHMEM_BCAST_SYNC_SIZE; i++)
+		pSync0[i] = _SHMEM_SYNC_VALUE;
+
+	/* Construct a random pattern and bcast it to all ranks */
+	int * proclist = shmalloc(sizeof(int)*(_world_size+1));
+	if (_world_rank == 0) {
+		for (int i = 0; i < _world_size; i++)
+			proclist[i] = i;
+		proclist[_world_size] = 0;
+		shuffle(proclist, (_world_size+1));
+	}    
+
+	shmem_broadcast32(proclist, proclist, (_world_size+1), 0, 0, 0, _world_size, pSync0);
+
+	long * sendbuf = shmalloc(nelems*SIZEOF_LONG);
+	long * recvbuf = shmalloc(nelems*SIZEOF_LONG);
+
+	for (int j = 0; j < DEFAULT_ITERS; j++) {
+		/* Initialize arrays */
+		for (int i = 0; i < nelems; i++) {
+			sendbuf[i] = i;
+			recvbuf[i] = -99;
+		}
+		shmem_barrier_all();
+
+		left_rank = proclist[(((_world_rank-1) < 0) ? _world_size-1 : (_world_rank-1))];
+		right_rank = proclist[(((_world_rank+1) > _world_size-1) ? 0 : (_world_rank+1))];
+		time_start = shmem_wtime();
+
+		if (_world_rank == left_rank) {		
+			shmem_fence();
+			shmem_long_put(recvbuf, sendbuf, nelems, right_rank);
+		}
+		/* Right rank waits till it receives the data */
+		if (_world_rank == right_rank) {
+			for (int i = 0; i < nelems; i++)
+				shmem_wait(&recvbuf[i], -99);
+		}
+		/* Repeat above, now other way round */
+		/* Re-Initialize arrays */
+		for (int i = 0; i < nelems; i++) {
+			sendbuf[i] = -99;
+		}
+		shmem_barrier_all();
+
+		if (_world_rank == right_rank) {		
+			shmem_fence();
+			shmem_long_put(recvbuf, sendbuf, nelems, right_rank);
+		}
+		/* Right rank waits till it receives the data */
+		if (_world_rank == left_rank) {
+			for (int i = 0; i < nelems; i++)
+				shmem_wait(&recvbuf[i], i);
+		}
+
+
+		time_end = shmem_wtime();
+	}
+	shmem_barrier_all();
+
+	/* Compute average by reducing this total_clock_time */
+	double clock_time_PE = time_end - time_start;
+	double total_clock_time, max_clock_time, min_clock_time;
+	shmem_double_sum_to_all(&total_clock_time, &clock_time_PE, 1, 
+			0, 0, _world_size, pWrk0, pSync0);
+	shmem_double_max_to_all(&max_clock_time, &clock_time_PE, 1, 
+			0, 0, _world_size, pWrk1, pSync1);
+	shmem_double_min_to_all(&min_clock_time, &clock_time_PE, 1, 
+			0, 0, _world_size, pWrk2, pSync2);    
+	//MPI_Reduce(&clock_time_PE, &total_clock_time, 1, MPI_DOUBLE, MPI_SUM, 0, SHMEM_COMM_WORLD);
+	if (_world_rank == 0) {
+		printf("Avg. Latency : %f, Avg. Bandwidth: %f MB/s for Message size: %lu bytes\n", (total_clock_time/_world_size), 
+				(double)((nelems*SIZEOF_LONG)/(double)((total_clock_time/_world_size) * 1024 * 1024)), nelems*SIZEOF_LONG);
+		printf("Max. Latency : %f, Min. Bandwidth: %f MB/s for Message size: %lu bytes\n", (max_clock_time), 
+				(double)((nelems*SIZEOF_LONG)/(double)(max_clock_time * 1024 * 1024)), nelems*SIZEOF_LONG);
+		printf("Min. Latency : %f, Max. Bandwidth: %f MB/s for Message size: %lu bytes\n", (min_clock_time), 
+				(double)((nelems*SIZEOF_LONG)/(double)(min_clock_time * 1024 * 1024)), nelems*SIZEOF_LONG);
+	}
+
+	shfree(sendbuf);
+	shfree(recvbuf);
+	shfree(proclist);
+	return;
+
+}
 /* target_rank = world_size - world_rank - 1 */
 void link_contended_lbw (unsigned int msg_size /* actual msg size is 2^msg_size */)
 {
@@ -462,29 +579,35 @@ int main(int argc, char * argv[])
 			break;
 		case 3:
 			if (_world_rank == 0)
+				printf("Random ring test on %d PEs\n",_world_size);
+			for (int i = 1; i < DEFAULT_SIZE; i*=2)
+				random_ring_lbw (i);
+			break;
+		case 4:
+			if (_world_rank == 0)
 				printf("Link contended test on %d PEs\n",_world_size);
 			for (int i = 1; i < DEFAULT_SIZE; i*=2)
 				link_contended_lbw (i);
 			break;
-		case 4:
+		case 5:
 			if (_world_rank == 0)
 				printf("Scatter test on %d PEs\n",_world_size);
 			for (int i = 1; i < DEFAULT_SIZE; i*=2)
 				scatter_gather_lbw (0, 0, 1, i);
 			break;
-		case 5:
+		case 6:
 			if (_world_rank == 0)
 				printf("Gather test on %d PEs\n",_world_rank);
 			for (int i = 1; i < DEFAULT_SIZE; i*=2)
 				scatter_gather_lbw (0, 0, 0, i);
 			break;
-		case 6:
+		case 7:
 			if (_world_rank == 0)
 				printf("All-to-all test on %d PEs\n",_world_rank);
 			for (int i = 1; i < DEFAULT_SIZE; i*=2)
 				a2a_lbw (0, i);
 			break;
-		case 7:
+		case 8:
 			if (_world_rank == 0)
 				printf("Ping-Pong tests on %d PEs\n",_world_size);
 			for (int i = 1; i < DEFAULT_SIZE; i*=2)
@@ -493,6 +616,10 @@ int main(int argc, char * argv[])
 				printf("Natural ring test on %d PEs\n",_world_size);
 			for (int i = 1; i < DEFAULT_SIZE; i*=2)
 				natural_ring_lbw (i);
+			if (_world_rank == 0)
+				printf("Random ring test on %d PEs\n",_world_size);
+			for (int i = 1; i < DEFAULT_SIZE; i*=2)
+				random_ring_lbw (i);
 			if (_world_rank == 0)
 				printf("Link contended test on %d PEs\n",_world_size);
 			for (int i = 1; i < DEFAULT_SIZE; i*=2)
@@ -511,7 +638,7 @@ int main(int argc, char * argv[])
 				a2a_lbw (0, i);
 			break;
 		default:
-			printf("Enter a number between 1-7\n");
+			printf("Enter a number between 1-8\n");
 	}
 
 	return 0;
