3ef989e688b9e7b035a796966cc153d37a1a6fe4
Jeff Hammond <jeff.science@gmail.com>
2013-10-25 11:38:35 -0500

    remove internal state

--- shmem.h
+++ shmem.h
@@ -51,9 +51,6 @@ typedef MPI_Aint shmem_offset_t;
 #define SHMEM_BCAST_SYNC_SIZE _SHMEM_BCAST_SYNC_SIZE
 #define _SHMEM_SYNC_VALUE 0
 
-//extern int     shmem_sheap_size;
-//extern void *  shmem_sheap_current_ptr;
-
 /* 8.1: Initialization Routines */
 void start_pes(int npes);
 
