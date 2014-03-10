b8e59b6bdfa90a11278b7f2b1b2fc42b2b311abf
Jeff Hammond <jeff.science@gmail.com>
2014-03-10 16:29:02 -0400

    more skeletons for extensions

--- src/shmemx.h
+++ src/shmemx.h
@@ -8,10 +8,11 @@ double shmem_wtime(void);
 char* shmem_nodename(void);
 
 #if EXTENSION_FINAL_ABORT
+#warning TODO
 #endif
 
 #if EXTENSION_COUNTING_PUT
-/* Signalling puts */
+#warning TODO
 typedef char * shmem_ct_t;
 
 void shmem_putmem_ct(shmem_ct_t ct, void *target, const void *source, size_t len, int pe);
@@ -22,25 +23,49 @@ void shmem_ct_set(shmem_ct_t ct, long value);
 void shmem_ct_wait(shmem_ct_t ct, long wait_for);
 #endif
 
+#if EXTENSION_INTEL_CONTEXTS
+#warning TODO
+typedef char * shmem_ctx_t;
+
+shmem_ctx_create(int num_ctx, int hint, shmem_ctx_t ctx[]);
+shmem_ctx_destroy(int num_ctx, shmem_ctx_t ctx[]);
+
+hmem_ctx_fence(shmem_ctx_t ctx);
+shmem_ctx_quiet(shmem_ctx_t ctx);
+shmem_ctx_barrier_all(shmem_ctx_t ctx);
+shmem_ctx_barrier(shmem_ctx_t ctx, int PE_start, int logPE_stride, int PE_size, long * pSync);
+
+shmem_ctx_putmem(shmem_ctx_t ctx, void * target, const void * source, size_t len, int pe);
+shmem_ctx_getmem(shmem_ctx_t ctx, void * target, const void * source, size_t len, int pe);
+/* ...AND SO FORTH */
+#endif
+
 #if EXTENSION_CRAY_INIT
+#warning TODO
 #endif
 
 #if EXTENSION_CRAY_THREADS
+#warning TODO
 #endif
 
 #if EXTENSION_ORNL_ASET
+#warning TODO
 #endif
 
 #if EXTENSION_ORNL_NBCOLL
+#warning TODO
 #endif
 
 #if EXTENSION_ORNL_NBRMA
+#warning TODO
 #endif
 
 #if EXTENSION_ARMCI_STRIDED
+#warning TODO
 #endif
 
 #if EXTENSION_INIT_SUBCOMM
+#warning TODO
 #endif
 
 #endif /* OSHMPI_SHMEMX_H */
