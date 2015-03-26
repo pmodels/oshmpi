6fc9daffd461d4c9e6b1c7e38a65ca419da0d4e0
Jeff Hammond <jeff.science@gmail.com>
2015-03-26 11:30:29 -0700

    add shmemx prefix to everything except the portals4-shmem externsions

--- src/shmemx.h
+++ src/shmemx.h
@@ -13,30 +13,30 @@ char* shmem_nodename(void);
 
 #if EXTENSION_COUNTING_PUT
 #error TODO
-typedef char * shmem_ct_t;
+typedef char * shmemx_ct_t;
 
-void shmem_putmem_ct(shmem_ct_t ct, void *target, const void *source, size_t len, int pe);
-void shmem_ct_create(shmem_ct_t *ct);
-void shmem_ct_free(shmem_ct_t *ct);
-long shmem_ct_get(shmem_ct_t ct);
-void shmem_ct_set(shmem_ct_t ct, long value);
-void shmem_ct_wait(shmem_ct_t ct, long wait_for);
+void shmemx_putmem_ct(shmem_ct_t ct, void *target, const void *source, size_t len, int pe);
+void shmemx_ct_create(shmem_ct_t *ct);
+void shmemx_ct_free(shmem_ct_t *ct);
+long shmemx_ct_get(shmem_ct_t ct);
+void shmemx_ct_set(shmem_ct_t ct, long value);
+void shmemx_ct_wait(shmem_ct_t ct, long wait_for);
 #endif
 
 #if EXTENSION_INTEL_CONTEXTS
 #error TODO
-typedef char * shmem_ctx_t;
+typedef char * shmemx_ctx_t;
 
-shmem_ctx_create(int num_ctx, int hint, shmem_ctx_t ctx[]);
-shmem_ctx_destroy(int num_ctx, shmem_ctx_t ctx[]);
+shmemx_ctx_create(int num_ctx, int hint, shmem_ctx_t ctx[]);
+shmemx_ctx_destroy(int num_ctx, shmem_ctx_t ctx[]);
 
-shmem_ctx_fence(shmem_ctx_t ctx);
-shmem_ctx_quiet(shmem_ctx_t ctx);
-shmem_ctx_barrier_all(shmem_ctx_t ctx);
-shmem_ctx_barrier(shmem_ctx_t ctx, int PE_start, int logPE_stride, int PE_size, long * pSync);
+shmemx_ctx_fence(shmem_ctx_t ctx);
+shmemx_ctx_quiet(shmem_ctx_t ctx);
+shmemx_ctx_barrier_all(shmem_ctx_t ctx);
+shmemx_ctx_barrier(shmem_ctx_t ctx, int PE_start, int logPE_stride, int PE_size, long * pSync);
 
-shmem_ctx_putmem(shmem_ctx_t ctx, void * target, const void * source, size_t len, int pe);
-shmem_ctx_getmem(shmem_ctx_t ctx, void * target, const void * source, size_t len, int pe);
+shmemx_ctx_putmem(shmem_ctx_t ctx, void * target, const void * source, size_t len, int pe);
+shmemx_ctx_getmem(shmem_ctx_t ctx, void * target, const void * source, size_t len, int pe);
 /* ...AND SO FORTH */
 #endif
 
