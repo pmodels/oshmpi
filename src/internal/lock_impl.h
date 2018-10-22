03636ed484950ba2f406d9dc3bb468fbf5180282
Min Si <msi@anl.gov>
2018-10-22 10:29:20 -0500

    Added internal lock impl file (empty)

---
+++ src/internal/lock_impl.h
@@ -0,0 +1,15 @@
+/* -*- Mode: C; c-basic-offset:4 ; -*- */
+/*
+ * (C) 2018 by Argonne National Laboratory.
+ *     See COPYRIGHT in top-level directory.
+ */
+
+#ifndef INTERNAL_LOCK_IMPL_H
+#define INTERNAL_LOCK_IMPL_H
+
+#include <shmem.h>
+#include "oshmpi_impl.h"
+
+
+
+#endif /* INTERNAL_LOCK_IMPL_H */
