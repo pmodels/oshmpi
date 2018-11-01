e8eb91b8176bef1595ff722569b6648f347c1b7c
Min Si <msi@anl.gov>
2018-11-01 16:51:46 -0500

    ctx: always return nonzero in ctx_create

--- src/shmem/context.c
+++ src/shmem/context.c
@@ -8,11 +8,15 @@
 
 int shmem_ctx_create(long options, shmem_ctx_t * ctx)
 {
-    OSHMPI_ERR_ABORT("Unsupported function: %s\n", __FUNCTION__);
-    return 0;
+    /* Return nonzero value if context cannot be created.
+     * We cannot support it in OSHMPI unless MPI exposes the communication
+     * resource management to users (e.g., endpoint).
+     *
+     * Spec v1.4 defines: an unsuccessful context creation call is not treated
+     * as an error and the OpenSHMEM library remains in a correct state. */
+    return SHMEM_NO_CTX;
 }
 
 void shmem_ctx_destroy(shmem_ctx_t ctx)
 {
-    OSHMPI_ERR_ABORT("Unsupported function: %s\n", __FUNCTION__);
 }
