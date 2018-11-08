d1a4af6a14ecd7ed28270f81f20324a0c2f0d314
Min Si <msi@anl.gov>
2018-11-08 12:45:34 -0600

    test: added threaded init test and output format
    Any error will cause global_exit; if all tests pass, PE 0
    will print "Passed" after finalize.

---
+++ tests/threaded_init.c
@@ -0,0 +1,49 @@
+/* -*- Mode: C; c-basic-offset:4 ; -*- */
+/*
+ * (C) 2018 by Argonne National Laboratory.
+ *     See COPYRIGHT in top-level directory.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <shmem.h>
+
+#if defined(SHMEM_REQUIRE_THREAD_SERIALIZED)
+int required = SHMEM_THREAD_SERIALIZED;
+const char *required_str = "SHMEM_THREAD_SERIALIZED";
+#elif defined(SHMEM_REQUIRE_THREAD_FUNNELED)
+int required = SHMEM_THREAD_FUNNELED;
+const char *required_str = "SHMEM_THREAD_FUNNELED";
+#elif defined(SHMEM_REQUIRE_THREAD_SINGLE)
+int required = SHMEM_THREAD_SINGLE;
+const char *required_str = "SHMEM_THREAD_SINGLE";
+#else /* Default THREAD_MULTIPLE */
+int required = SHMEM_THREAD_MULTIPLE;
+const char *required_str = "SHMEM_THREAD_MULTIPLE";
+#endif
+
+int main(int argc, char *argv[])
+{
+    int provided = 0;
+    int mype = -1;
+
+    shmem_init_thread(required, &provided);
+
+    if (provided != required) {
+        fprintf(stderr, "Requested %s (0x%x), but provided 0x%x\n",
+                required_str, required, provided);
+        fflush(stderr);
+        shmem_global_exit(-1);
+    }
+
+    mype = shmem_my_pe();
+    shmem_finalize();
+
+    /* Check local routines only */
+    if (mype == 0) {
+        fprintf(stdout, "Passed\n");
+        fflush(stderr);
+    }
+
+    return 0;
+}
