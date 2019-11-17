647ef0c1aab677b59f69fba960e200d931b763c7
Min Si <msi@anl.gov>
2019-11-17 16:53:44 -0700

    test: add hybrid shmem+mpi test with varying init order

--- tests/hybrid_mpi/Makefile.mk
+++ tests/hybrid_mpi/Makefile.mk
@@ -5,4 +5,23 @@
 check_PROGRAMS +=  \
     hybrid_mpi/mapping_id            \
     hybrid_mpi/query_mpi_progress    \
-    hybrid_mpi/threaded
+    hybrid_mpi/threaded              \
+    hybrid_mpi/init_shmem_init_shmem_finalize       \
+    hybrid_mpi/init_shmem_init_mpi_finalize         \
+    hybrid_mpi/init_mpi_init_mpi_finalize           \
+    hybrid_mpi/init_mpi_init_shmem_finalize
+
+# Default SHMEM init first SHMEM finalize first
+hybrid_mpi_init_shmem_init_shmem_finalize_SOURCES   = hybrid_mpi/init.c
+hybrid_mpi_init_shmem_init_shmem_finalize_CPPFLAGS  = $(AM_CPPFLAGS)
+
+# Default SHMEM init first
+hybrid_mpi_init_shmem_init_mpi_finalize_SOURCES   = hybrid_mpi/init.c
+hybrid_mpi_init_shmem_init_mpi_finalize_CPPFLAGS  = -DTEST_MPI_FINALIZE_FIRST $(AM_CPPFLAGS)
+
+hybrid_mpi_init_mpi_init_mpi_finalize_SOURCES   = hybrid_mpi/init.c
+hybrid_mpi_init_mpi_init_mpi_finalize_CPPFLAGS  = -DTEST_MPI_INIT_FIRST -DTEST_MPI_FINALIZE_FIRST $(AM_CPPFLAGS)
+
+# Default SHMEM finalize first
+hybrid_mpi_init_mpi_init_shmem_finalize_SOURCES   = hybrid_mpi/init.c
+hybrid_mpi_init_mpi_init_shmem_finalize_CPPFLAGS  = -DTEST_MPI_INIT_FIRST $(AM_CPPFLAGS)
---
+++ tests/hybrid_mpi/init.c
@@ -0,0 +1,55 @@
+/* -*- Mode: C; c-basic-offset:4 ; -*- */
+/*
+ * (C) 2018 by Argonne National Laboratory.
+ *     See COPYRIGHT in top-level directory.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <shmem.h>
+
+int main(int argc, char *argv[])
+{
+#ifdef TEST_MPI_INIT_FIRST
+    MPI_Init(&argc, &argv);
+    fprintf(stdout, "1. MPI_Init called\n");
+
+    shmem_init();
+    fprintf(stdout, "2. shmem_init called\n");
+#else
+    shmem_init();
+    fprintf(stdout, "1. shmem_init called\n");
+
+    /* MPI may already be initialized in shmem_init. */
+    int mpi_initialized = 0;
+    MPI_Initialized(&mpi_initialized);
+    if (!mpi_initialized)
+        MPI_Init(&argc, &argv);
+
+    fprintf(stdout, "2. MPI_Init %s\n", mpi_initialized ? "skipped" : "called");
+#endif
+
+#ifdef TEST_MPI_FINALIZE_FIRST
+    /* OSHMPI does not support this order. */
+    MPI_Finalize();
+    fprintf(stdout, "3. MPI_Finalize called\n");
+
+    shmem_finalize();
+    fprintf(stdout, "4. shmem_finalize called\n");
+#else
+    shmem_finalize();
+    fprintf(stdout, "3. shmem_finalize called\n");
+
+    /* MPI may already be finalized in shmem_finalize. */
+    int mpi_finalized = 0;
+    MPI_Finalized(&mpi_finalized);
+    if (!mpi_finalized)
+        MPI_Finalize();
+    fprintf(stdout, "4. MPI_Finalize %s\n", mpi_finalized ? "skipped" : "called");
+#endif
+
+    fprintf(stdout, "Passed\n");
+    fflush(stdout);
+
+    return 0;
+}
